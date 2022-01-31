43ce3f2d1c967351c1f3d1851eaac065d51feb21
Harkishen-Singh <harkishensingh@hotmail.com>
2022-01-31 19:04:55 +0530

    Update mod, add new converted blocks along with benchmark test.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- go.mod
+++ go.mod
@@ -1,19 +1,19 @@
-module github.com/Harkishen-Singh/prom-postings-comparison
+module github.com/Harkishen-Singh/prom-posting-comparison
 
 go 1.17
 
 require (
-	github.com/Harkishen-Singh/prometheus-index-roaringbitmaps v0.0.0-20220106080035-7f40fda24215
+	github.com/Harkishen-Singh/prometheus v1.8.2-0.20220131133216-e722b3a4ca9e
 	github.com/dgraph-io/sroar v0.0.0-20211209113350-3e3f1b382a64
 	github.com/go-kit/log v0.2.0
 	github.com/pkg/errors v0.9.1
-	github.com/prometheus/prometheus v1.8.2-0.20211203003126-b9b57f9900dd
+	github.com/prometheus/prometheus v1.8.2-0.20220131061416-9fde6edbf562
 	github.com/stretchr/testify v1.7.0
 )
 
 require (
-	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
-	github.com/aws/aws-sdk-go v1.42.22 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/aws/aws-sdk-go v1.42.31 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -27,17 +27,17 @@ require (
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/common/sigv4 v0.1.0 // indirect
-	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/goleak v1.1.12 // indirect
-	golang.org/x/net v0.0.0-20211123203042-d83791d6bcd9 // indirect
+	golang.org/x/net v0.0.0-20220105145211-5b0dc2dfae98 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- postings_bench_test.go
+++ postings_bench_test.go
@@ -7,19 +7,19 @@ import (
 	"os"
 	"testing"
 
-	"github.com/dgraph-io/sroar"
 	"github.com/go-kit/log"
 	"github.com/stretchr/testify/require"
 
-	"github.com/prometheus/prometheus/model/labels"
-	"github.com/prometheus/prometheus/storage"
+	h_labels "github.com/Harkishen-Singh/prometheus/model/labels"
+	p_labels "github.com/prometheus/prometheus/model/labels"
 
-	rb_tsdb "github.com/Harkishen-Singh/prometheus-index-roaringbitmaps/tsdb"
-	rb_index "github.com/Harkishen-Singh/prometheus-index-roaringbitmaps/tsdb/index"
+	h_storage "github.com/Harkishen-Singh/prometheus/storage"
+	p_storage "github.com/prometheus/prometheus/storage"
 
+	rb_tsdb "github.com/Harkishen-Singh/prometheus/tsdb"
+	rb_chunkenc "github.com/Harkishen-Singh/prometheus/tsdb/chunkenc"
 	be_tsdb "github.com/prometheus/prometheus/tsdb"
 	be_chunkenc "github.com/prometheus/prometheus/tsdb/chunkenc"
-	be_index "github.com/prometheus/prometheus/tsdb/index"
 )
 
 func generateSeriesIds(start, end, incr int) []uint32 {
@@ -42,200 +42,200 @@ func getBigEndianPostings(ids []uint32) *bigEndianPostings {
 	return newBigEndianPostings(bSlice1)
 }
 
-func BenchmarkIntersection(b *testing.B) {
-	increments := []int{2, 3, 4, 6, 8, 10, 12, 14, 16, 18, 20}
-	seriesIds := make([][]uint32, len(increments))
-	for i, incr := range increments {
-		seriesIds[i] = generateSeriesIds(1, 1000, incr)
-	}
-
-	// BigEndian postings.
-	bigEndianPs := make([]Postings, 0)
-	for i := range seriesIds {
-		bigEndianPs = append(bigEndianPs, getBigEndianPostings(seriesIds[i]))
-	}
-	numBigEndian := 0
-	b.Run("Intersect_BigEndianPostings", func(b *testing.B) {
-		p, err := ExpandPostings(Intersect(bigEndianPs...))
-		require.NoError(b, err)
-		if numBigEndian == 0 {
-			numBigEndian = len(p)
-		}
-	})
-
-	// Roaring bitmap postings.
-	seriesIdsUint64 := make([][]uint64, len(seriesIds))
-	for i := range seriesIds {
-		tmp := make([]uint64, 0, len(seriesIds[i]))
-		for j := range seriesIds[i] {
-			tmp = append(tmp, uint64(seriesIds[i][j]))
-		}
-		seriesIdsUint64[i] = tmp
-	}
-	roaringBitmapPs := make([]*sroar.Bitmap, 0)
-	for i := range seriesIds {
-		roaringBitmapPs = append(roaringBitmapPs, newRoarBitmap(seriesIdsUint64[i]...))
-	}
-
-	numRoaring := 0
-	b.Run("Intersect_RoaringBitmapsPostings", func(b *testing.B) {
-		p := roaringIntersect(roaringBitmapPs...)
-		numRoaring = p.GetCardinality()
-	})
-
-	require.Equal(b, numBigEndian, numRoaring)
-}
-
-const be_index_path = "data/big_endian_index"
-const rb_index_path = "data/roaring_bitmap_index"
-
-func BenchmarkIntersectionRealIndex(b *testing.B) {
-	c_be := 0
-	b.Run("Intersect_big_endian_real_index", func(b *testing.B) {
-		irBE, err := be_index.NewFileReader(be_index_path)
-		require.NoError(b, err)
-		defer irBE.Close()
-
-		bePostings_1, err := irBE.Postings("job", "prometheus")
-		require.NoError(b, err)
-
-		bePostings_2, err := irBE.Postings("job", "promscale")
-		require.NoError(b, err)
-
-		bePostings_3, err := irBE.Postings("job", "demo")
-		require.NoError(b, err)
-
-		bePostings_4, err := irBE.Postings("job", "robust")
-		require.NoError(b, err)
-
-		p := be_index.Intersect(bePostings_1, bePostings_2, bePostings_3, bePostings_4)
-		for p.Next() {
-			c_be++
-		}
-	})
-
-	c_rb := 0
-	b.Run("Intersect_roaring_bitmap_real_index", func(b *testing.B) {
-		irRB, err := rb_index.NewFileReader(rb_index_path)
-		require.NoError(b, err)
-		defer irRB.Close()
-
-		rbPostings_1, err := irRB.Postings("job", "prometheus")
-		require.NoError(b, err)
-
-		rbPostings_2, err := irRB.Postings("job", "promscale")
-		require.NoError(b, err)
-
-		rbPostings_3, err := irRB.Postings("job", "demo")
-		require.NoError(b, err)
-
-		rbPostings_4, err := irRB.Postings("job", "robust")
-		require.NoError(b, err)
-
-		p := rb_index.Intersect(rbPostings_1, rbPostings_2, rbPostings_3, rbPostings_4)
-		for p.Next() {
-			c_rb++
-		}
-	})
-
-	require.Equal(b, c_be, c_rb)
-}
-
-func BenchmarkUnionRealIndex(b *testing.B) {
-	c_be := 0
-	b.Run("Union_big_endian_real_index", func(b *testing.B) {
-		irBE, err := be_index.NewFileReader(be_index_path)
-		require.NoError(b, err)
-		defer irBE.Close()
-
-		bePostings_1, err := irBE.Postings("job", "prometheus")
-		require.NoError(b, err)
-
-		bePostings_2, err := irBE.Postings("job", "promscale")
-		require.NoError(b, err)
-
-		bePostings_3, err := irBE.Postings("job", "demo")
-		require.NoError(b, err)
-
-		bePostings_4, err := irBE.Postings("job", "robust")
-		require.NoError(b, err)
-
-		p := be_index.Merge(bePostings_1, bePostings_2, bePostings_3, bePostings_4)
-		for p.Next() {
-			c_be++
-		}
-	})
-
-	c_rb := 0
-	b.Run("Union_roaring_bitmap_real_index", func(b *testing.B) {
-		irRB, err := rb_index.NewFileReader(rb_index_path)
-		require.NoError(b, err)
-		defer irRB.Close()
-
-		rbPostings_1, err := irRB.Postings("job", "prometheus")
-		require.NoError(b, err)
-
-		rbPostings_2, err := irRB.Postings("job", "promscale")
-		require.NoError(b, err)
-
-		rbPostings_3, err := irRB.Postings("job", "demo")
-		require.NoError(b, err)
-
-		rbPostings_4, err := irRB.Postings("job", "robust")
-		require.NoError(b, err)
-
-		p := rb_index.Merge(rbPostings_1, rbPostings_2, rbPostings_3, rbPostings_4)
-		for p.Next() {
-			c_rb++
-		}
-	})
-
-	require.Equal(b, c_be, c_rb)
-}
-
-func BenchmarkUnion(b *testing.B) {
-	increments := []int{2, 3, 4, 6, 8, 10}
-	seriesIds := make([][]uint32, len(increments))
-	for i, incr := range increments {
-		seriesIds[i] = generateSeriesIds(1, 100, incr)
-	}
-
-	// BigEndian postings.
-	bigEndianPs := make([]Postings, 0)
-	for i := range seriesIds {
-		bigEndianPs = append(bigEndianPs, getBigEndianPostings(seriesIds[i]))
-	}
-	numBigEndian := 0
-	b.Run("Union_BigEndianPostings", func(b *testing.B) {
-		p, err := ExpandPostings(Merge(bigEndianPs...))
-		require.NoError(b, err)
-		if numBigEndian == 0 {
-			numBigEndian = len(p)
-		}
-	})
-
-	// Roaring bitmap postings.
-	seriesIdsUint64 := make([][]uint64, len(seriesIds))
-	for i := range seriesIds {
-		tmp := make([]uint64, 0, len(seriesIds[i]))
-		for j := range seriesIds[i] {
-			tmp = append(tmp, uint64(seriesIds[i][j]))
-		}
-		seriesIdsUint64[i] = tmp
-	}
-	roaringBitmapPs := make([]*sroar.Bitmap, 0)
-	for i := range seriesIds {
-		roaringBitmapPs = append(roaringBitmapPs, newRoarBitmap(seriesIdsUint64[i]...))
-	}
-
-	numRoaring := 0
-	b.Run("Union_RoaringBitmapsPostings", func(b *testing.B) {
-		p := roaringUnion(roaringBitmapPs...)
-		numRoaring = p.GetCardinality()
-	})
-
-	require.Equal(b, numBigEndian, numRoaring)
-}
+//func BenchmarkIntersection(b *testing.B) {
+//	increments := []int{2, 3, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+//	seriesIds := make([][]uint32, len(increments))
+//	for i, incr := range increments {
+//		seriesIds[i] = generateSeriesIds(1, 1000, incr)
+//	}
+//
+//	// BigEndian postings.
+//	bigEndianPs := make([]Postings, 0)
+//	for i := range seriesIds {
+//		bigEndianPs = append(bigEndianPs, getBigEndianPostings(seriesIds[i]))
+//	}
+//	numBigEndian := 0
+//	b.Run("Intersect_BigEndianPostings", func(b *testing.B) {
+//		p, err := ExpandPostings(Intersect(bigEndianPs...))
+//		require.NoError(b, err)
+//		if numBigEndian == 0 {
+//			numBigEndian = len(p)
+//		}
+//	})
+//
+//	// Roaring bitmap postings.
+//	seriesIdsUint64 := make([][]uint64, len(seriesIds))
+//	for i := range seriesIds {
+//		tmp := make([]uint64, 0, len(seriesIds[i]))
+//		for j := range seriesIds[i] {
+//			tmp = append(tmp, uint64(seriesIds[i][j]))
+//		}
+//		seriesIdsUint64[i] = tmp
+//	}
+//	roaringBitmapPs := make([]*sroar.Bitmap, 0)
+//	for i := range seriesIds {
+//		roaringBitmapPs = append(roaringBitmapPs, newRoarBitmap(seriesIdsUint64[i]...))
+//	}
+//
+//	numRoaring := 0
+//	b.Run("Intersect_RoaringBitmapsPostings", func(b *testing.B) {
+//		p := roaringIntersect(roaringBitmapPs...)
+//		numRoaring = p.GetCardinality()
+//	})
+//
+//	require.Equal(b, numBigEndian, numRoaring)
+//}
+//
+//const be_index_path = "data/be_block_copy/index"
+//const rb_index_path = "data/rb_block_copy/01FTR26BGTB9SDS8W56N8HZGGG/index"
+//
+//func BenchmarkIntersectionRealIndex(b *testing.B) {
+//	c_be := 0
+//	b.Run("Intersect_big_endian_real_index", func(b *testing.B) {
+//		irBE, err := be_index.NewFileReader(be_index_path)
+//		require.NoError(b, err)
+//		defer irBE.Close()
+//
+//		bePostings_1, err := irBE.Postings("job", "prometheus")
+//		require.NoError(b, err)
+//
+//		bePostings_2, err := irBE.Postings("job", "promscale")
+//		require.NoError(b, err)
+//
+//		bePostings_3, err := irBE.Postings("job", "demo")
+//		require.NoError(b, err)
+//
+//		bePostings_4, err := irBE.Postings("job", "robust")
+//		require.NoError(b, err)
+//
+//		p := be_index.Intersect(bePostings_1, bePostings_2, bePostings_3, bePostings_4)
+//		for p.Next() {
+//			c_be++
+//		}
+//	})
+//
+//	c_rb := 0
+//	b.Run("Intersect_roaring_bitmap_real_index", func(b *testing.B) {
+//		irRB, err := rb_index.NewFileReader(rb_index_path)
+//		require.NoError(b, err)
+//		defer irRB.Close()
+//
+//		rbPostings_1, err := irRB.Postings("job", "prometheus")
+//		require.NoError(b, err)
+//
+//		rbPostings_2, err := irRB.Postings("job", "promscale")
+//		require.NoError(b, err)
+//
+//		rbPostings_3, err := irRB.Postings("job", "demo")
+//		require.NoError(b, err)
+//
+//		rbPostings_4, err := irRB.Postings("job", "robust")
+//		require.NoError(b, err)
+//
+//		p := rb_index.Intersect(rbPostings_1, rbPostings_2, rbPostings_3, rbPostings_4)
+//		for p.Next() {
+//			c_rb++
+//		}
+//	})
+//
+//	require.Equal(b, c_be, c_rb)
+//}
+//
+//func BenchmarkUnionRealIndex(b *testing.B) {
+//	c_be := 0
+//	b.Run("Union_big_endian_real_index", func(b *testing.B) {
+//		irBE, err := be_index.NewFileReader(be_index_path)
+//		require.NoError(b, err)
+//		defer irBE.Close()
+//
+//		bePostings_1, err := irBE.Postings("job", "prometheus")
+//		require.NoError(b, err)
+//
+//		bePostings_2, err := irBE.Postings("job", "promscale")
+//		require.NoError(b, err)
+//
+//		bePostings_3, err := irBE.Postings("job", "demo")
+//		require.NoError(b, err)
+//
+//		bePostings_4, err := irBE.Postings("job", "robust")
+//		require.NoError(b, err)
+//
+//		p := be_index.Merge(bePostings_1, bePostings_2, bePostings_3, bePostings_4)
+//		for p.Next() {
+//			c_be++
+//		}
+//	})
+//
+//	c_rb := 0
+//	b.Run("Union_roaring_bitmap_real_index", func(b *testing.B) {
+//		irRB, err := rb_index.NewFileReader(rb_index_path)
+//		require.NoError(b, err)
+//		defer irRB.Close()
+//
+//		rbPostings_1, err := irRB.Postings("job", "prometheus")
+//		require.NoError(b, err)
+//
+//		rbPostings_2, err := irRB.Postings("job", "promscale")
+//		require.NoError(b, err)
+//
+//		rbPostings_3, err := irRB.Postings("job", "demo")
+//		require.NoError(b, err)
+//
+//		rbPostings_4, err := irRB.Postings("job", "robust")
+//		require.NoError(b, err)
+//
+//		p := rb_index.Merge(rbPostings_1, rbPostings_2, rbPostings_3, rbPostings_4)
+//		for p.Next() {
+//			c_rb++
+//		}
+//	})
+//
+//	require.Equal(b, c_be, c_rb)
+//}
+//
+//func BenchmarkUnion(b *testing.B) {
+//	increments := []int{2, 3, 4, 6, 8, 10}
+//	seriesIds := make([][]uint32, len(increments))
+//	for i, incr := range increments {
+//		seriesIds[i] = generateSeriesIds(1, 100, incr)
+//	}
+//
+//	// BigEndian postings.
+//	bigEndianPs := make([]Postings, 0)
+//	for i := range seriesIds {
+//		bigEndianPs = append(bigEndianPs, getBigEndianPostings(seriesIds[i]))
+//	}
+//	numBigEndian := 0
+//	b.Run("Union_BigEndianPostings", func(b *testing.B) {
+//		p, err := ExpandPostings(Merge(bigEndianPs...))
+//		require.NoError(b, err)
+//		if numBigEndian == 0 {
+//			numBigEndian = len(p)
+//		}
+//	})
+//
+//	// Roaring bitmap postings.
+//	seriesIdsUint64 := make([][]uint64, len(seriesIds))
+//	for i := range seriesIds {
+//		tmp := make([]uint64, 0, len(seriesIds[i]))
+//		for j := range seriesIds[i] {
+//			tmp = append(tmp, uint64(seriesIds[i][j]))
+//		}
+//		seriesIdsUint64[i] = tmp
+//	}
+//	roaringBitmapPs := make([]*sroar.Bitmap, 0)
+//	for i := range seriesIds {
+//		roaringBitmapPs = append(roaringBitmapPs, newRoarBitmap(seriesIdsUint64[i]...))
+//	}
+//
+//	numRoaring := 0
+//	b.Run("Union_RoaringBitmapsPostings", func(b *testing.B) {
+//		p := roaringUnion(roaringBitmapPs...)
+//		numRoaring = p.GetCardinality()
+//	})
+//
+//	require.Equal(b, numBigEndian, numRoaring)
+//}
 
 const be_blockpath = "data/be_block"
 const rb_blockpath = "data/rb_block"
@@ -245,8 +245,8 @@ func ConvertBigEndianBlockToRoaringBitmapBLock(t *testing.T) {
 	querier, err := be_tsdb.NewBlockQuerier(block, 0, 1641945600000)
 	require.NoError(t, err)
 
-	matcher := labels.MustNewMatcher(labels.MatchRegexp, "__name__", ".+")
-	seriesSet := querier.Select(false, &storage.SelectHints{
+	matcher := p_labels.MustNewMatcher(p_labels.MatchRegexp, "__name__", ".+")
+	seriesSet := querier.Select(false, &p_storage.SelectHints{
 		Start: 0,
 		End:   1641945600000,
 		Step:  1,
@@ -261,10 +261,10 @@ func ConvertBigEndianBlockToRoaringBitmapBLock(t *testing.T) {
 		lbls := serie.Labels()
 		itr := serie.Iterator()
 		seriesAppender := blockWriter.Appender(context.Background())
-		seriesRef := storage.SeriesRef(0)
+		cl := convertLabels(lbls)
 		for itr.Next() {
 			ts, v := itr.At()
-			seriesRef, err = seriesAppender.Append(seriesRef, lbls, ts, v)
+			_, err = seriesAppender.Append(0, cl, ts, v)
 			require.NoError(t, err)
 		}
 		require.NoError(t, seriesAppender.Commit())
@@ -272,4 +272,127 @@ func ConvertBigEndianBlockToRoaringBitmapBLock(t *testing.T) {
 	ulid, err := blockWriter.Flush(context.Background())
 	require.NoError(t, err)
 	fmt.Println("roaring bitmap index block ulid", ulid)
+}
+
+func convertLabels(p p_labels.Labels) h_labels.Labels {
+	hLabels := []h_labels.Label{}
+	for i := range p {
+		l := h_labels.Label{
+			Name: p[i].Name,
+			Value: p[i].Value,
+		}
+		hLabels = append(hLabels, l)
+	}
+	return hLabels
+}
+
+type benchQueries struct {
+	id int
+	pmatchers []*p_labels.Matcher
+	hmatchers []*h_labels.Matcher
+}
+
+var queries = []benchQueries{
+	{
+		id: 1,
+		pmatchers: []*p_labels.Matcher{
+			p_labels.MustNewMatcher(p_labels.MatchRegexp, "__name__", ".+"),
+		},
+		hmatchers: []*h_labels.Matcher{
+			h_labels.MustNewMatcher(h_labels.MatchRegexp, "__name__", ".+"),
+		},
+	},
+	{
+		id: 2,
+		pmatchers: []*p_labels.Matcher{
+			p_labels.MustNewMatcher(p_labels.MatchEqual, "job", "prometheus"),
+			p_labels.MustNewMatcher(p_labels.MatchEqual, "__name__", "go_goroutines"),
+		},
+		hmatchers: []*h_labels.Matcher{
+			h_labels.MustNewMatcher(h_labels.MatchEqual, "job", "prometheus"),
+			h_labels.MustNewMatcher(h_labels.MatchEqual, "__name__", "go_goroutines"),
+		},
+	},
+	{
+		id: 3,
+		pmatchers: []*p_labels.Matcher{
+			p_labels.MustNewMatcher(p_labels.MatchEqual, "job", "prometheus"),
+			p_labels.MustNewMatcher(p_labels.MatchNotEqual, "__name__", "go_goroutines"),
+		},
+		hmatchers: []*h_labels.Matcher{
+			h_labels.MustNewMatcher(h_labels.MatchEqual, "job", "prometheus"),
+			h_labels.MustNewMatcher(h_labels.MatchNotEqual, "__name__", "go_goroutines"),
+		},
+	},
+}
+
+func BenchmarkPromQLQueries(b *testing.B) {
+	// BigEndian block.
+	be_block_path := "data/be_block_copy"
+	be_block, err := be_tsdb.OpenBlock(log.NewLogfmtLogger(os.Stdout), be_block_path, be_chunkenc.NewPool())
+	require.NoError(b, err)
+
+	be_querier, err := be_tsdb.NewBlockQuerier(be_block, 0, 1641945600000)
+	require.NoError(b, err)
+
+	// Bitmap block.
+	rb_block_path := "data/rb_block_copy/01FTR26BGTB9SDS8W56N8HZGGG"
+	rb_block, err := rb_tsdb.OpenBlock(log.NewLogfmtLogger(os.Stdout), rb_block_path, rb_chunkenc.NewPool())
+	require.NoError(b, err)
+
+	rb_querier, err := rb_tsdb.NewBlockQuerier(rb_block, 0, 1641945600000)
+	require.NoError(b, err)
+
+	var be_series_set p_storage.SeriesSet
+	var rb_series_set h_storage.SeriesSet
+
+	for i := range queries {
+		b.Run("big_endian", func(b *testing.B) {
+			be_series_set = be_querier.Select(false, nil, queries[i].pmatchers...)
+		})
+		b.Run("roaring_bitmap", func(b *testing.B) {
+			rb_series_set = rb_querier.Select(false, nil, queries[i].hmatchers...)
+		})
+	}
+
+	// Verify similar outputs.
+	for {
+		containsBE := be_series_set.Next()
+		containsRB := rb_series_set.Next()
+		require.Equal(b, containsBE, containsRB)
+		if !containsRB || !containsBE {
+			break
+		}
+
+		require.NoError(b, be_series_set.Err())
+		require.NoError(b, rb_series_set.Err())
+
+		seriesSetBE := be_series_set.At()
+		seriesSetRB := rb_series_set.At()
+
+		labelsBE := seriesSetBE.Labels()
+		labelsRB := seriesSetRB.Labels()
+		require.Equal(b, convertLabels(labelsBE), labelsRB)
+
+		//itrBE := seriesSetBE.Iterator()
+		//itrRB := seriesSetRB.Iterator()
+		//for {
+		//	hasMoreBE := itrBE.Next()
+		//	hasMoreRB := itrRB.Next()
+		//	require.Equal(b, hasMoreBE, hasMoreRB)
+		//
+		//	if !hasMoreRB || !hasMoreBE {
+		//		break
+		//	}
+		//
+		//	tsBE, vBE := itrBE.At()
+		//	tsRB, vRB := itrRB.At()
+		//	require.Equal(b, tsBE, tsRB)
+		//	if math.IsNaN(vBE) {
+		//		require.True(b, math.IsNaN(vRB))
+		//		continue
+		//	}
+		//	require.EqualValues(b, vBE, vRB)
+		//}
+	}
 }
